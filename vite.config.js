fbbc755455ae779c166257325ffedacbca3d03fa
unknown <edgardollentas04@gmail.com>
2026-01-09 18:25:24 +0800

    Prepare frontend for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,27 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/scss/index.scss',
-                'resources/scss/admin/adminlogin.scss',
-                'resources/scss/admin/admindashboard.scss',
-                'resources/scss/admin/adminemployee.scss',
-                'resources/scss/user/userlogin.scss',
-                'resources/scss/user/userregister.scss',
-                'resources/scss/sidebar.scss',
-                'resources/scss/modal.scss',
-                'resources/js/admin/dashboard.js',
-                'resources/js/admin/employee.js',
-                'resources/js/admin/user.js',
-                'resources/js/user/verification.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  build: {
+    outDir: 'dist', // <-- use 'dist' for Vercel
+    emptyOutDir: true,
+  },
+  plugins: [
+    laravel({
+      input: [
+        'resources/scss/index.scss',
+        'resources/scss/admin/adminlogin.scss',
+        'resources/scss/admin/admindashboard.scss',
+        'resources/scss/admin/adminemployee.scss',
+        'resources/scss/user/userlogin.scss',
+        'resources/scss/user/userregister.scss',
+        'resources/scss/sidebar.scss',
+        'resources/scss/modal.scss',
+        'resources/js/admin/dashboard.js',
+        'resources/js/admin/employee.js',
+        'resources/js/admin/user.js',
+        'resources/js/user/verification.js',
+      ],
+      refresh: true,
+    }),
+  ],
 });
